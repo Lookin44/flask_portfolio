fc832467c325d3b2f15d65fafb94f10aa9eb136a
lookin44 <look-in44@yandex.ru>
2022-04-28 13:59:05 +0300

    Upgrade project structure

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
-app.config.from_pyfile('settings.py')
+app.config.from_pyfile('../settings.py')
 
+bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
 
 from blog import routes
-
